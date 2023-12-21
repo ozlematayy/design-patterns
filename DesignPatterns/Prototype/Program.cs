151312c5f54030cc4bb8a54b2dd5703638a6f348
Özlem <77455910+ozlematayy@users.noreply.github.com>
2023-12-22 01:55:22 +0300

    Prototype design pattern code

---
+++ DesignPatterns/Prototype/Program.cs
@@ -0,0 +1,42 @@
+﻿#region Prototype
+public abstract class Logger
+{
+    public abstract Logger Clone();
+    public abstract void Log(string message);
+    public string LogPath { get; set; }
+
+}
+#endregion
+
+#region Concrete Prototype
+public class FileLogger : Logger
+{
+    public override void Log(string message)
+    {
+        Console.WriteLine($"File Logger: {message}");
+    }
+    public override Logger Clone()
+    {
+        return (FileLogger)MemberwiseClone();
+    }
+}
+#endregion
+
+#region Running the code - Client
+class Client
+{
+    static void Main(string[] args)
+    {
+        FileLogger file1 = new FileLogger()
+        {
+            LogPath = "log.txt"
+        };
+        file1.Log("Log message");
+        Console.WriteLine($"File1 Log Path : {file1.LogPath}");
+
+        var file2 = file1.Clone();
+        Console.WriteLine($"File2 Log Path : {file2.LogPath}");
+    }
+}
+
+#endregion
