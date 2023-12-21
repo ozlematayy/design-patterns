aa7df0b516d8cfa1de6ef3459468724a3f787030
Özlem <77455910+ozlematayy@users.noreply.github.com>
2023-12-22 00:38:19 +0300

    Singleton design pattern code

---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,30 @@
+﻿#region Usage
+var singleton = Singleton.Instance();
+singleton.Log();
+#endregion
+
+#region Singleton Definition
+class Singleton
+{
+    private static Singleton _singleton;
+    static object _lockObj = new Object();
+    private Singleton(){}
+
+    public static Singleton Instance()
+    {
+        lock(_lockObj)
+        {
+            if(_singleton == null)
+            {
+                _singleton = new Singleton();
+            }
+        }
+        return _singleton;
+    }
+    public void Log()
+    {
+        Console.WriteLine("Object creation process has been successfully completed.");
+    }
+}
+#endregion
+
